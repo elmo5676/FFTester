51d8207848a3169d1d105952f7279573622fa816
4bitCrew <matt@4bitcrew.com>
2020-02-25 13:01:26 -0800

    Initial Commit

---
+++ FFTester/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FFTester
+//
+//  Created by Matthew Elmore on 2/25/20.
+//  Copyright © 2020 Matthew Elmore. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
